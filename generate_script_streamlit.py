670bf03099a05b1d451c46799d5ca18d4cbcdadf
Mikhail <mihail.pol1999@gmml.com>
2023-10-26 20:49:37 +0300

    v 0.1

--- generate_script_streamlit.py
+++ generate_script_streamlit.py
@@ -5,20 +5,34 @@ from transformers import pipeline
 from transformers import GPT2LMHeadModel
 from transformers import GPT2Tokenizer
 
+
+@st.cache_resource()
+def load_generator(path, _tkn):
+    model = GPT2LMHeadModel.from_pretrained(path)
+    return pipeline('text-generation', model=model, tokenizer=_tkn, device=model.device)
+
+@st.cache_resource()
+def load_tokenizer():
+    return GPT2Tokenizer.from_pretrained('gpt2')
+
+@st.cache_data()
+def load_all_lines(path):
+    return pd.read_csv(path)
+
+
+
 st.title('This is GPT-2 model fine-tuned with Star Trek scipts')
 st.markdown('''Write a line for  model to begin  with and it will try it's best to continue it as Star Trek script. Please write caracter name in CAPS.''')
 
-tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
-model = GPT2LMHeadModel.from_pretrained(f'''{dirname(__file__)}/models/all_scripts''')
-generator = pipeline('text-generation', model=model, tokenizer=tokenizer, device=model.device)
+tokenizer = load_tokenizer()
+generator = load_generator(f'''{dirname(__file__)}/models/all_scripts''', tokenizer)
+all_lines = load_all_lines(f'{dirname(__file__)}/STscripts/all_lines')
 
-if 'al' not in st.session_state:
-    st.session_state['al'] = pd.read_csv(f'{dirname(__file__)}/STscripts/all_lines')
 if 'input_val' not in st.session_state:
     st.session_state['input_val'] = '''PICARD: You will agree, Data, that Starfleet's orders are difficult?'''
 
 def random_val():
-    st.session_state['input_val'] = st.session_state['al'].sample(n=1)['0'].item()
+    st.session_state['input_val'] = all_lines.sample(n=1)['0'].item()
 
 random_line = st.button('Start with random line from scripts database', on_click=random_val)
 
